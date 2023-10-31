c747770c4f0d8cb20b7289edf6d91d8b91ba2628
melli55 <mgellis2020@gmail.com>
2023-10-31 10:24:38 -0500

    Added media queries for Contact/Meet pages
    added medai queries, fixed contact forum,

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // JS scripts placed here
 
+
+/* Navigation Bar*/
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -10,7 +12,10 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
   }
 
-  let slideIndex = 1;
+
+
+/* Slideshow */
+let slideIndex = 1;
   showSlides(slideIndex);
   
   // Next/previous controls
